98803781d0aa6c3b787f8ad9bb043f923441aa54
nguyenkinhthanh <duongthiminh0111@gmail.com>
2023-03-08 13:39:10 +0700

    nop bai tap lab1-MSV:PD07562

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,12 +1,13 @@
 //nhap gia tri cho a la mot so nguyen tu ban phim
 var a = prompt("nhap vao so nguyen a: ");
-//ep kieu gia tri a tu string thanh so nguyen
+//ep kieu gia tri a tu string sang so nguyen
 var a1 = parseInt(a);
 //nhap gia tri cho b la mot so nguyen tu ban phim
 var b = prompt("nhap vao so nguyen b: ");
+//ep kieu gia tri b tu string sang so nguyen
 var b1 = parseInt(b);
-var c = a1+b1;
 //tinh tong hai so a,b
+var c = a1+b1;
 //in thong tin ra man hinh su dung document
 document.writeln("Gia tri cua c = "+(c));
 //in thong tin ra man hinh su dung console.log
