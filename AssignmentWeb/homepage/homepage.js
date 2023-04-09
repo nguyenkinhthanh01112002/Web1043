0fa50d0942e7021dc9153377b23326f78d6329b0
nguyenkinhthanh01112002 <nguyenkinhthanh11@gmail.com>
2023-04-09 13:37:45 +0700

    Nop Asignment GD 2

--- AssignmentWeb/homepage/homepage.js
+++ AssignmentWeb/homepage/homepage.js
@@ -25,4 +25,35 @@ function removeOnmousemove(elem) {
   elem.classList.remove("onmousemoveLogo:hover");
 }
 
-
+const form = document.getElementById('contact-form');
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const name = nameInput.value.trim();
+      const email = emailInput.value.trim();
+      const phone = phoneInput.value.trim();
+  
+      // Kiểm tra tên không để trống
+      if (name === '') {
+        alert('Please enter your name.');
+        return;
+      }
+  
+      // Kiểm tra email theo định dạng chính xác
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+      }
+  
+      // Kiểm tra số điện thoại chỉ chứa số và không vượt quá 11 kí tự
+      if (!/^[0-9]{11}$/.test(phone)) {
+        alert('Please enter a valid phone number.');
+        return;
+      }
+  
+      // Thực hiện gửi form ở đây...
+      alert('Form submitted successfully!');
+    });
